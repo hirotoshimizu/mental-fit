f51c3d7a5a301156513dfd73075bb6e22a27bf76
hirotoshimizu <hshimizu@LAPTOP-TSFN3RIP.localdomain>
2022-04-13 17:32:35 +0900

    Added custom user model

---
+++ app/review/urls.py
@@ -0,0 +1,13 @@
+# from importlib.resources import path
+# from unicodedata import name
+
+# from django.urls import path
+# from .views import Index
+
+
+# app_name = "review"
+
+
+# urlpatterns = [
+#     path("", Index.as_view(), name="index"),
+# ]
---
+++ app/review/views.py
@@ -0,0 +1,5 @@
+# from django.views.generic import ListView
+
+
+# class Index(ListView):
+#     template_name = "review/list.html"
